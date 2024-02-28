19f10469281756a8f00675092939c04f72a65d90
David Morales <morales.david1997@gmail.com>
2024-02-28 09:33:26 -0400

    fix(api): update MerchantId middleware to handle multiple formats of Authorization header

--- api/middlewares/merchant_id.go
+++ api/middlewares/merchant_id.go
@@ -1,15 +1,31 @@
 package middlewares
 
 import (
+	"log"
+	"strings"
+
 	"github.com/gin-gonic/gin"
 	"github.com/masterkeysrd/online-payment-platform/internal/domain/merchant"
 )
 
 func MerchantId(service merchant.Service) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		secretKey := c.GetHeader("Authorization")
+		authorization := c.GetHeader("Authorization")
+
+		fields := strings.Fields(authorization)
+
+		var secretKey string
+		if len(fields) == 2 {
+			secretKey = fields[1]
+		} else {
+			secretKey = fields[0]
+		}
+
+		log.Println("secretKey", secretKey)
 
 		if secretKey == "" {
+			c.JSON(401, gin.H{"error": "Unauthorized"})
+			c.Abort()
 			return
 		}
 
