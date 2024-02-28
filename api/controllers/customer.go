fc32eb198f41b665c085c6f430d7be5c99c38443
David Morales <morales.david1997@gmail.com>
2024-02-27 20:31:23 -0400

    feat(api): add payment methods endpoints to CustomerController

--- api/controllers/customer.go
+++ api/controllers/customer.go
@@ -15,10 +15,31 @@ func NewCustomerController() *CustomerController {
 
 func (c *CustomerController) RegisterRoutes(router *gin.RouterGroup) {
 	router.POST("", c.Create)
+	router.GET("/:customerId/payments-methods", c.GetPaymentMethods)
+	router.POST("/:customerId/payments-methods", c.AddPaymentMethod)
+	router.GET("/:customerId/payments-methods/:paymentMethodId", c.GetPaymentMethod)
 }
 
 func (c *CustomerController) Create(ctx *gin.Context) {
 	ctx.JSON(http.StatusCreated, gin.H{
 		"message": "Customer created",
+	})
+}
+
+func (c *CustomerController) GetPaymentMethods(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Get payment methods",
+	})
+}
+
+func (c *CustomerController) AddPaymentMethod(ctx *gin.Context) {
+	ctx.JSON(http.StatusCreated, gin.H{
+		"message": "Payment method added",
+	})
+}
+
+func (c *CustomerController) GetPaymentMethod(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": "Get payment method",
 	})
 }
